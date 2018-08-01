4d2f526036e8dee91b523a5c5b661c6dd3c0c5a4
Phil Barker <phil@pjjk.net>
2018-08-01 14:28:42 +0100

    rm traces of old theme based wikidata taxonomies

--- functions.php
+++ functions.php
@@ -2,7 +2,7 @@
 /**
  * @license GPL 2.0+
  */
- 
+
 defined( 'ABSPATH' ) or die( 'Be good. If you can\'t be good be careful' );
 define('PB_HIDE_COVER_PROMO', true);
 
@@ -12,6 +12,3 @@ function omniana_theme_setup() {
 add_action( 'after_setup_theme', 'omniana_theme_setup' );
 
 $omniana_dir = get_stylesheet_directory();
-//include_once( $omniana_dir.'/inc/taxonomies.php' );
-//include_once( $omniana_dir.'/inc/taxonomy_theme_functions.php' );
-//include_once( $omniana_dir.'/inc/wikidata_functions.php' );
